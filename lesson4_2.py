91656e505b6854556d8221c94a2099a3e8153375
葉威廷 <161096413+xbroth14@users.noreply.github.com>
2025-07-19 16:06:11 +0800

    塞入新資料

---
+++ lesson4_2.py
@@ -0,0 +1,19 @@
+#list
+fruits = ["apple","banana","cherry"]
+fruits.append("orange")
+fruits.append("raspberry")
+fruits.append("orange")  # 新增元素
+fruits.append("grape")  # 再新增一個元素
+fruits.append("watermelon")
+fruits.append("pineapple")
+fruits.append("mango")
+fruits.append("kiwi")
+fruits.append("strawberry")
+fruits.append("blueberry")
+fruits.append("peach")
+fruits.append("pear")
+fruits.append("plum")
+fruits.append("lemon")
+print(type(fruits))
+print("水果清單",fruits)
+
